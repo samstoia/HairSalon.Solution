eb3d1317da04630ee2b6a7edce9d4b3738dba21c
samstoia <samstoia@gmail.com>
2019-03-08 12:18:38 -0800

    start building out specialty view/controller/model

--- HairSalon/Controllers/SpecialtiesController.cs
+++ HairSalon/Controllers/SpecialtiesController.cs
@@ -7,6 +7,26 @@ namespace HairSalon.Controllers
 {
 	public class SpecialtiesController: Controller
 	{
-    
+    [HttpGet("/specialties")]
+    public ActionResult Index()
+    {
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      return View(allSpecialties);
+    }
+
+    [HttpGet("/specialties/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/specialties")]
+    public ActionResult Create(string name)
+    {
+      Specialty newSpecialty = new Specialty(name);
+      newSpecialty.Save();
+      List<Specialty> allSpecialties = Specialty.GetAll();
+      return View("Index", allSpecialties);
+    }
   }
 }
--- HairSalon/Models/Specialty.cs
+++ HairSalon/Models/Specialty.cs
@@ -6,6 +6,23 @@ namespace HairSalon.Models
 {
   public class Specialty
   {
+    private int _id;
+		private string _specialtyName;
+		public Specialty(string specialtyName, int id = 0)
+		{
+			_id = id;
+			_specialtyName = specialtyName;
+		}
+
+		public string GetSpecialtyName()
+		{
+			return _specialtyName;
+		}
+
+		public int GetId()
+		{
+			return _id;
+		}
     
   }
 }
